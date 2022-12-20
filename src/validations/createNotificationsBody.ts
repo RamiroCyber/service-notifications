14029d2075f45078b95de09e866181a3736a0be1
RamiroCyber <ramiro.dr@gmail.com>
2022-12-19 22:20:23 -0300

    validacoes simples

--- src/validations/createNotificationsBody.ts
+++ src/validations/createNotificationsBody.ts
@@ -1,6 +1,15 @@
+import { IsNotEmpty, IsUUID, Length } from "class-validator";
+
 export class CreateNotificationsBody {
+  @IsNotEmpty()
+  @IsUUID()
   recipienteId: string;
+
+  @IsNotEmpty()
+  @Length(5,300)
   content: string;
+
+  @IsNotEmpty()
   category: string;
 
 }
